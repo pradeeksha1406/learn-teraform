11813729253662fa6750cddc58eff60d2cac0b2b
pradeep <p.guta@gmail.com>
2024-03-14 08:53:41 +0530

    ss

--- Expense-v2/app-create/main.tf
+++ Expense-v2/app-create/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "instance" {
   ami                    = local.ami
-  instance_type          = "t3.micro"
+  instance_type          = "t2.micro"
   vpc_security_group_ids = [data.aws_security_group.sg.id]
 
   tags = {
